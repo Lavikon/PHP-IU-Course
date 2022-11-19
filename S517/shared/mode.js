92ca7a65b4bea42f0e0a8f7ddb582fce5afb7695
Niko Lavikainen <108793040+Lavikon@users.noreply.github.com>
2022-11-19 08:44:05 +0200

    Add files via upload

---
+++ S517/shared/mode.js
@@ -0,0 +1,5 @@
+// Javascript for changing the light and dark mode
+function setStyleSheet(url) {
+    var stylesheet = document.getElementById("colorsheet");
+    stylesheet.setAttribute('href', url);
+}
